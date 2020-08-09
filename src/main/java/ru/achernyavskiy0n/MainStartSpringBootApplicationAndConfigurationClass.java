6383057994206ff6b54e37279ab00f82077fba58
AntonChernyavskiy <chernyavskiy.in@gmail.com>
2020-08-09 06:54:42 +0300

    init

---
+++ src/main/java/ru/achernyavskiy0n/MainStartSpringBootApplicationAndConfigurationClass.java
@@ -0,0 +1,12 @@
+package ru.achernyavskiy0n;
+
+/**
+ * 09.08.2020
+ *
+ * @author a.chernyavskiy0n
+ */
+public class MainStartSpringBootApplicationAndConfigurationClass {
+    public static void main(String[] args) {
+
+    }
+}
